d796f87d3d6a91388404914e2068a693be8de2a4
Swapnil Anuse <youremail@gmail.com>
2026-01-22 14:25:24 +0530

    donetoday

---
+++ swapp1.java
@@ -0,0 +1,13 @@
+class swapp1
+{
+    public static void main(String[]args)
+    {
+        int a=20,b=30;
+        System.out.println("Befor Swapping two nmbers\n"+a+" "+b);
+
+        int t=a;
+            a=b;
+            b=t;
+            System.out.println("Afte swapping two num bers\n"+a+" "+b);
+    }
+}
