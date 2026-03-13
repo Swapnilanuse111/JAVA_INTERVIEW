df36fe35e8a45ed15a9a19e82f5d1383839b2292
Swapnil Anuse <youremail@gmail.com>
2026-03-13 21:54:54 +0530

    done

---
+++ exception1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class exception1
+{
+    public static void main(String args[])
+    {
+        try{
+            int a=10,b=0;
+            int c=a/b;
+            System.out.println("Result: "+c);
+        }
+        catch(ArithmeticException e)
+        {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+}
